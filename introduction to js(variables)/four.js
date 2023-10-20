2cda595f2ea05c4241f99d783704e8bfa18ed720
masai-abhay <148424787+masai-abhay@users.noreply.github.com>
2023-10-20 14:42:35 +0530

    addedd

---
+++ introduction to js(variables)/four.js
@@ -0,0 +1,18 @@
+let school = "🄼🄰🅂🄰🄸 🅂🄲🄷🄾🄾🄻"
+let rollno = 1
+let grade = "A"
+let section = "c"
+let math = 50
+let physics = 60
+let chemistry = 65
+
+console.log(school)
+console.log ( "rollno -" + rollno)
+console.log("grade -" + grade)
+console.log("section -" + section)
+console.log("math -" + math)
+console.log("physics -" + physics)
+console.log("chemistry -" + chemistry)
+
+
+
---
+++ introduction to js(variables)/two.js
@@ -0,0 +1,9 @@
+//let name = "abhay chand"
+//console.log(name)
+
+//let father  = "govind chand"
+//console.log(father)
+
+//let mother = "mohini devi"
+//console.log(mother)
+
